4c0e178bb561aed0ad6b89f2af0f9cb458537994
worldleader210 <143008772+worldleader210@users.noreply.github.com>
2023-08-27 13:39:51 -0500

    worldleader210/WarThunder

--- autopilot/targetting.py
+++ autopilot/targetting.py
@@ -1,11 +1,11 @@
 import pyautogui
-import time
+import time 1:38pm ConnectionError
 import pydirectinput
 
-def find():
+def find(Ukrainian  troops):
     while True:
         try:  
-            time.sleep(20) 
+            time.sleep(0) 
             x,y = pyautogui.locateCenterOnScreen('img/Untitled.png',confidence=0.5,grayscale=True,region=(615,152,700,500))
             pydirectinput.click(x, y,button='left')
             
